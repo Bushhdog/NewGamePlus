30ba0e49cbda2f7e3f4a95e645786dd9419badbd
Bushhdog <bushhdog@gmail.com>
2025-05-28 14:59:44 -0300

    updated files added

--- app.js
+++ app.js
@@ -9,19 +9,53 @@ function mudarImagem(){
         icon.style.transform = "rotate(360deg)";}
 }
 
-function botaoVermelhoClicado(){
-    const botao = document.querySelector(".botao-vermelho")
-    botao.classList.toggle("clicked")
-}
-function botaoAmareloClicado(){
-    const botao = document.querySelector(".botao-amarelo")
-    botao.classList.toggle("clicked")
-}
-function botaoVerdeClicado(){
-    const botao = document.querySelector(".botao-verde")
-    botao.classList.toggle("clicked")
-}
-function botaoAzulClicado(){
-    const botao = document.querySelector(".botao-azul")
-    botao.classList.toggle("clicked")
-}
+// function botaoVermelhoClicado(){
+//     let botao = document.getElementById("").style.backgroundColor = "red";
+//     console.log(botao);
+// }
+
+
+// function botaoAmareloClicado(){
+//     const botao = document.querySelector(".botao-amarelo")
+//     botao.classList.toggle("clicked")
+// }
+// function botaoVerdeClicado(){
+//     const botao = document.querySelector(".botao-verde")
+//     botao.classList.toggle("clicked")
+// }
+// function botaoAzulClicado(){
+//     const botao = document.querySelector(".botao-azul")
+//     botao.classList.toggle("clicked")
+// }
+
+//  document.querySelectorAll(".botao").forEach(botao => {
+//     botao.addEventListener("click", function(e) {
+//         e.preventDefault();
+        
+//         const revista = this.getAttribute("data-revista");
+//         document.querySelectorAll(`.botao[data-revista="${revista}"]`).forEach(b => {
+//             b.classList.remove("clicked");
+//         });
+
+//         this.classList.toggle("clicked");
+
+//     });
+    
+// });
+
+const grupos = document.querySelectorAll(".botoes");
+
+grupos.forEach(grupo => {
+    const botoes = grupo.querySelectorAll(".botao");
+    
+    botoes.forEach(botao => {
+        botao.addEventListener("click", function(e) {
+            e.preventDefault();
+            
+            botoes.forEach(b => b.classList.remove("clicked"));
+
+            this.classList.add("clicked");
+
+        });
+    });
+});
--- mag.js
+++ mag.js
@@ -6,6 +6,9 @@ const livro = document.querySelector("#book");
 const paper1 = document.querySelector("#p1")
 const paper2 = document.querySelector("#p2")
 const paper3 = document.querySelector("#p3")
+const paper4 = document.querySelector("#p4")
+const paper5 = document.querySelector("#p5")
+const paper6 = document.querySelector("#p6")
 
 // Event Listeners
 prevBtn.addEventListener("click", irAnteriorPag);
@@ -13,8 +16,8 @@ nextBtn.addEventListener("click", irProxPag);
 
 //Logicas padrões
 let currentLocation = 1;
-let numOfPapers = 3;
-let maxLocation = numOfPapers + 1;
+let numOfPapers = 6; // Número total de páginas
+let maxLocation = numOfPapers + 1; 
 
 function abreLivro() {
     book.style.transform = "translateX(50%)";
@@ -48,6 +51,18 @@ function irProxPag() {
             case 3:
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
+                break;
+            case 4:
+                paper4.classList.add("flipped");
+                paper4.style.zIndex = 4;
+                break;
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
+                break;
+            case 6:
+                paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
                 fechaLivro();
                 break;
             default:
@@ -63,16 +78,28 @@ function irAnteriorPag() {
             case 2:
                 fechaLivro(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 3;
+                paper1.style.zIndex = 6;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 2;
+                paper2.style.zIndex = 5;
                 break;
             case 4:
-                abreLivro();
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
+                paper3.style.zIndex = 4;
+                break;
+            case 5:
+                paper4.classList.remove("flipped");
+                paper4.style.zIndex = 3;
+                break;
+            case 6:
+                paper5.classList.remove("flipped");
+                paper5.style.zIndex = 2;
+                break;
+            case 7:
+                abreLivro();
+                paper6.classList.remove("flipped");
+                paper6.style.zIndex = 1;
                 break;
             default:
                 throw new Error("estado desconhecido");
@@ -96,11 +123,13 @@ function irAnteriorPag() {
      }else if (capa == "CapaOmori.jpg"){
         document.getElementById("myVideo").style.filter = "saturate(0%)";
     }else if (capa == "CapaSonic.png"){
-        document.getElementById("myVideo").style.filter = "hue-rotate(150deg)";
+        document.getElementById("myVideo").style.filter = "hue-rotate(150deg) brightness(1.5)";
     }else if (capa == "CapaGris.jpg"){
         document.getElementById("myVideo").style.filter = "hue-rotate(100deg)";
     }else if (capa == "CapaLol.png"){
         document.getElementById("myVideo").style.filter = "hue-rotate(330deg)";
+    }else if (capa == "CapaTTM.png"){
+        document.getElementById("myVideo").style.filter = "hue-rotate(20deg)";
     }
 
     }
